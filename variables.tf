89edb22ff45450b51f77ae693e9c66b43b4c60a0
hfiaz <57761242+hfiaz@users.noreply.github.com>
2020-01-23 10:24:55 +0000

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_id" {
+  default = "vpc-059456953092de3aa"
+}
+variable "name" {
+  default = "hussain-app-"
+}
+variable "name_db" {
+  default = "hussain-db-"
+}
+variable "app_ami_id" {
+  default = "ami-04eb5b71ba5dfae90"
+}
+variable "mongo_ami_id" {
+  default = "ami-0172b711b0aa87cae"
+}
